6309ba08dff99fd429c3b33c9fcc46009b82cf27
LAZE4ER <89158736+LAZE4ER@users.noreply.github.com>
2026-02-15 17:16:54 +0200

    DZ11

--- DZ10/users/views.py
+++ DZ10/users/views.py
@@ -5,6 +5,9 @@ from .forms import UserRegisterationForm
 from core import settings
 from django.template.loader import render_to_string
 # Create your views here.
+
+
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterationForm(request.POST)
@@ -13,20 +16,22 @@ def register(request):
             
             send_mail(
                 subject="New User Registration",
-                message = f'User {form.cleaned_data["username"]} has registered with email {form.cleaned_data["email"]}',
+                message=f'User {form.cleaned_data["username"]} has registered with email {form.cleaned_data["email"]}',
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 recipient_list=[settings.ADMIN_EMAIL]
             )
+            
             html_content = render_to_string(
-                "emails/user_welcome.html",
+                "users/user_welcome.html",
                 {"username": form.cleaned_data['username']}
             )
+            
             email = EmailMessage(
                 subject="Welcome to Our Site",
                 body=html_content,
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 to=[form.cleaned_data['email']],
-                cc = [settings.ADMIN_EMAIL],
+                cc=[settings.ADMIN_EMAIL],
             )
             email.content_subtype = "html"
             email.send()
--- DZ5/forms/forms.py
+++ DZ5/forms/forms.py
@@ -5,14 +5,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-
-class RegisterForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2', 'email']
-        help_texts = {
-            'username': '',
-        }
 class AdminFormRegister(UserCreationForm):
     secret_token = forms.CharField(
         label="Secret Token",
